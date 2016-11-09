62508699fcaa4b87f6cdcd98882a8286560bdb79
Paul Uhn <paul.uhn@motorolasolutions.com>
2016-11-09 15:25:26 -0600

    Initial Commit

---
+++ RealmWriteTestFramework/RealmWriteTestFramework.h
@@ -0,0 +1,19 @@
+//
+//  RealmWriteTestFramework.h
+//  RealmWriteTestFramework
+//
+//  Created by Paul Uhn on 11/9/16.
+//  Copyright © 2016 Paul Uhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmWriteTestFramework.
+FOUNDATION_EXPORT double RealmWriteTestFrameworkVersionNumber;
+
+//! Project version string for RealmWriteTestFramework.
+FOUNDATION_EXPORT const unsigned char RealmWriteTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmWriteTestFramework/PublicHeader.h>
+
+
